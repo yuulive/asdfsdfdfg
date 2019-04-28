7528773094b3f519cea378a75d7265565dcdf774
daingun <>
2019-04-28 22:31:03 +0200

    Add documentation

--- src/linear_system/mod.rs
+++ src/linear_system/mod.rs
@@ -3,6 +3,7 @@ use num_complex::Complex64;
 
 use std::fmt;
 
+/// State-space representation of a linar system
 #[derive(Debug)]
 pub struct Ss {
     a: DMatrix<f64>,
@@ -11,28 +12,50 @@ pub struct Ss {
     d: DMatrix<f64>,
 }
 
+/// Implementation of the methods for the state-space
 impl Ss {
+    /// Create a new state-space representation
+    ///
+    /// # Arguments
+    ///
+    /// * `a` - A matrix
+    /// * `b` - B matrix
+    /// * `c` - C matrix
+    /// * `d` - D matrix
     pub fn new(a: DMatrix<f64>, b: DMatrix<f64>, c: DMatrix<f64>, d: DMatrix<f64>) -> Self {
         Ss { a, b, c, d }
     }
 
+    /// Get the A matrix
     pub fn a(&self) -> DMatrix<f64> {
         self.a.clone()
     }
+
+    /// Get the C matrix
     pub fn b(&self) -> DMatrix<f64> {
         self.b.clone()
     }
+
+    /// Get the C matrix
     pub fn c(&self) -> DMatrix<f64> {
         self.c.clone()
     }
+
+    /// Get the D matrix
     pub fn d(&self) -> DMatrix<f64> {
         self.d.clone()
     }
 
+    /// Calculate the poles of the system
     pub fn poles(&self) -> DVector<Complex64> {
         Schur::new(self.a.clone()).complex_eigenvalues()
     }
 
+    /// Calculate the equilibrium point for the given input condition
+    ///
+    /// # Arguments
+    ///
+    /// * `u` - Input vector
     pub fn equilibrium(&self, u: &[f64]) -> Option<Equilibrium> {
         let u = DMatrix::from_row_slice(u.len(), 1, u);
         let inv_a = &self.a.clone().try_inverse().unwrap();
@@ -52,13 +75,23 @@ impl fmt::Display for Ss {
     }
 }
 
+/// Struct describing an equilibrium point
 #[derive(Debug)]
 pub struct Equilibrium {
+    /// State equilibrium
     x: DMatrix<f64>,
+    /// Output equilibrium
     y: DMatrix<f64>,
 }
 
+/// Implement methods for equilibrium
 impl Equilibrium {
+    /// Create a new equilibrium given the state and the output vectors
+    ///
+    /// # Arguments
+    ///
+    /// * `x` - State equilibrium
+    /// * `y` - Output equilibrium
     pub fn new(x: DMatrix<f64>, y: DMatrix<f64>) -> Self {
         Equilibrium { x, y }
     }
@@ -66,10 +99,6 @@ impl Equilibrium {
 
 impl fmt::Display for Equilibrium {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "x: {}\ny: {}",
-            self.x, self.y
-        )
+        write!(f, "x: {}\ny: {}", self.x, self.y)
     }
 }
