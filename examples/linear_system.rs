50ec401c82be2ca223dc6b99ff651bbb8ccfc939
daingun <>
2019-06-29 00:49:32 +0200

    Fix example output formatting

--- examples/linear_system.rs
+++ examples/linear_system.rs
@@ -16,24 +16,24 @@ fn main() {
     let poles = sys.poles();
 
     println!("{}", &sys);
-    println!("poles: {:?}", poles);
+    println!("poles:\n{:?}", poles);
 
     let u = 0.0;
     println!("\nEquilibrium for u={}", u);
     let eq = sys.equilibrium(&[u]).unwrap();
-    println!("x: {:?}\ny: {:?}", eq.x(), eq.y());
+    println!("x:\n{:?}\ny:\n{:?}", eq.x(), eq.y());
 
     println!("\nTransform linear system into a transfer function");
     let tf_matrix = TfMatrix::from(sys);
-    println!("Tf:{}", tf_matrix);
+    println!("Tf:\n{}", tf_matrix);
 
     println!("\nEvaluate transfer function in ω = 0.9");
     let u = vec![Complex::new(0.0, 0.9)];
     let y = tf_matrix.eval(&u);
-    println!("u:{:?}\ny:{:?}", &u, &y);
+    println!("u:\n{:?}\ny:\n{:?}", &u, &y);
 
     println!(
-        "y:{:?}",
+        "y:\n{:?}",
         &y.iter().map(|x| (x.norm(),x.arg().to_degrees())).collect::<Vec<_>>()
     );
 }
