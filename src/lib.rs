a19a1f0c1850e19e1ad37686ba6dc05921deebf4
daingun <>
2019-07-11 23:12:51 +0200

    Implement convertion to decibels

--- src/lib.rs
+++ src/lib.rs
@@ -33,10 +33,28 @@ where
     left.iter().zip(right).map(|(l, r)| f(l, r)).collect()
 }
 
+/// Trait for the conversion to decibels.
+pub trait Decibel<T> {
+    /// Convert to decibels
+    fn to_db(&self) -> T;
+}
+
+/// Implementation of the Decibels for f64
+impl Decibel<f64> for f64 {
+    /// Convert f64 to decibels
+    fn to_db(&self) -> f64 {
+        20. * self.log10()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use num_traits::ops::inv::Inv;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn decibel_test() {
+        assert_eq!(40., 100_f64.to_db());
+        assert_eq!(-3.0102999566398116, 2_f64.inv().sqrt().to_db());
     }
 }
