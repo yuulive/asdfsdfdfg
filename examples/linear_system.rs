5d33b244d01ebf8b1efac8368d08f56aec3585d9
daingun <>
2019-08-16 23:59:18 +0200

    Create step function in examples

--- examples/linear_system.rs
+++ examples/linear_system.rs
@@ -19,12 +19,13 @@ fn main() {
     println!("poles:\n{:?}", poles);
 
     println!("\nStep response:");
+    let step = |_| vec![1.0];
 
-    let rk2 = sys.rk2(|_| vec![1.], &[0., 0.], 0.1, 150);
+    let rk2 = sys.rk2(step, &[0., 0.], 0.1, 150);
     println!("rk2 stationary values: {:?}", rk2.last().unwrap());
     // Change to 'true' to print the result
     if false {
-        for i in sys.rk2(|_| vec![1.], &[0., 0.], 0.1, 150) {
+        for i in sys.rk2(step, &[0., 0.], 0.1, 150) {
             println!(
                 "{};{};{};{};{}",
                 i.time(),
@@ -36,11 +37,11 @@ fn main() {
         }
     }
 
-    let rk4 = sys.rk4(|_| vec![1.], &[0., 0.], 0.1, 150);
+    let rk4 = sys.rk4(step, &[0., 0.], 0.1, 150);
     println!("rk4 stationary values: {:?}", rk4.last().unwrap());
     // Change to 'true' to print the result
     if false {
-        for i in sys.rk4(|_| vec![1.], &[0., 0.], 0.1, 150) {
+        for i in sys.rk4(step, &[0., 0.], 0.1, 150) {
             println!(
                 "{};{};{};{};{}",
                 i.time(),
@@ -52,11 +53,11 @@ fn main() {
         }
     }
 
-    let rkf45 = sys.rkf45(|_| vec![1.], &[0., 0.], 0.1, 16., 1e-4);
+    let rkf45 = sys.rkf45(step, &[0., 0.], 0.1, 16., 1e-4);
     println!("rkf45 stationary values: {:?}", rkf45.last().unwrap());
     // Change to 'true' to print the result
     if false {
-        for i in sys.rkf45(|_| vec![1.], &[0., 0.], 0.1, 16., 1e-4) {
+        for i in sys.rkf45(step, &[0., 0.], 0.1, 16., 1e-4) {
             println!(
                 "{};{};{};{};{};{}",
                 i.time(),
@@ -69,11 +70,11 @@ fn main() {
         }
     }
 
-    let radau = sys.radau(|_| vec![1.], &[0., 0.], 0.1, 150, 1e-4);
+    let radau = sys.radau(step, &[0., 0.], 0.1, 150, 1e-4);
     println!("radau stationary values: {:?}", radau.last().unwrap());
     // Change to 'true' to print the result
     if false {
-        for i in sys.radau(|_| vec![1.], &[0., 0.], 0.1, 150, 1e-4) {
+        for i in sys.radau(step, &[0., 0.], 0.1, 150, 1e-4) {
             println!(
                 "{:>4.1};{:>9.6};{:>9.6};{:>9.6};{:>9.6}",
                 i.time(),
