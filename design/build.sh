a2cff1d1ae53675ab025368f19b8804c37b93f69
daingun <>
2020-11-10 22:22:03 +0100

    Update html documentation script and template
    Ref: #9

--- design/build.sh
+++ design/build.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
 
-pandoc -s -t html5 --template template.html5 -o public/index.html index.md
-pandoc -s -t html5 --template template.html5 -o public/introduction.html introduction.md
-pandoc -s -t html5 --template template.html5 -o public/description.html description.md
-pandoc -s -t html5 --template template.html5 -o public/requirements.html requirements.md
-pandoc -s -t html5 --template template.html5 -o public/test_plan.html test_plan.md
-pandoc -s -t html5 --template template.html5 -o public/continuous_integration.html continuous_integration.md
-pandoc -s -t html5 --template template.html5 -o public/references.html references.md
+for file in ./*.md ; do
+    if [ -e "$file" ]
+    then
+        # Remove the starting './'
+        f="${f#./}"
+        # Remove the ending '.md'
+        f="${file%.md}"
+        # echo "$f"
+        # pandoc -s -t html5 --toc --toc-depth=2 --template template.html5 -o public/"$f".html "$file"
+        pandoc \
+            --standalone \
+            --to=html5 \
+            --table-of-contents --toc-depth=2 \
+            --template=template.html5 \
+            --output=public/"$f".html \
+            "$file"
+    fi
+done
