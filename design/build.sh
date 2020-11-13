d8405c76c844bef70e737ee2309b3be06c4afdec
daingun <>
2020-11-14 00:47:55 +0100

    Update documentation build script to use GNU/parallel
    Ref: #9

--- design/build.sh
+++ design/build.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
 
-for file in ./*.md ; do
-    if [ -e "$file" ]
-    then
-        # Remove the starting './'
-        f="${f#./}"
-        # Remove the ending '.md'
-        f="${file%.md}"
-        pandoc \
-            --standalone \
-            --to=html5 \
-            --mathml \
-            --table-of-contents --toc-depth=2 \
-            --template=template.html5 \
-            --output=public/"$f".html \
-            "$file"
-    fi
-done
+find ./ -name "*.md" \
+    | parallel \
+          pandoc \
+          --standalone \
+          --to=html5 \
+          --mathml \
+          --table-of-contents --toc-depth=2 \
+          --template=template.html5 \
+          --output=public/{/.}.html \
+          {}
+
+# {} is the argument passed to parallel (file name)
+# {/.} is the file name argument passed to parallel, without directory and extension
